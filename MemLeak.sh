15d02f62557276d35d51fffc836c9bebf34d980d
mallius <mallius@qq.com>
2017-04-08 23:35:46 +0800

    valgrind

--- MemLeak.sh
+++ MemLeak.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #运行程序检查内存泄漏
-valgrind --leak-check=full --show-reachable=yes $1
+#valgrind --leak-check=full --show-reachable=yes $1
+valgrind --tool=memcheck --leak-check=yes --show-reachable=yes $1
